33070cca1cbbfbc8bfc7d92fc5d8e221ea16229e
martinabrle <71389217+martinabrle@users.noreply.github.com>
2020-11-05 15:25:17 +0100

    Add files via upload

---
+++ apis/poi/health_check_staging.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+declare url="https://openhackdz41qgg3poi.azurewebsites.net/api/healthcheck/poi"
+declare result=$(curl -i $url 2>/dev/null | grep HTTP/2)
+declare status
+if [[ -z $result ]];
+    then status="N/A"
+    else status=${result:7:3}
+fi
+echo $status
+if [[ $status -eq "200" ]];
+   then exit 0
+fi
+
+exit 1
